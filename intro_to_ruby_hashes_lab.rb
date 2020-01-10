25c44aa50a49a6761c5f0cbcbdc7f28f825f2eff
April Escobar <aprilfaith.e@gmail.com>
2020-01-10 07:49:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,12 @@ def monopoly_with_third_tier
  hash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {}
+      :rent_in_dollars => {
+        :one => 1,
+        :two => 2,
+        :three => 3,
+        :four => 4
+      }
     }
   }
   hash
